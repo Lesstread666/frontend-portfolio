c3670101f5e71c6bae6d0bf8970241ff736f31fa
Lesstread666 <vlamaks1604@gmail.com>
2026-01-08 17:17:21 +0100

    Fix format for name of the project + comments for the js file

--- js/github-projects.js
+++ js/github-projects.js
@@ -2,6 +2,14 @@ const latestProjectContainer = document.querySelector('#latest-project')
 const loadingState = document.querySelector('.loading')
 const gitHubApiUrl = 'https://api.github.com/users/Lesstread666/repos?sort=created&direction=desc'
 
+//Change the format for the project name (upperCase and replacing - to space)
+const formatRepoName = (name) => {
+    return name
+        .replace(/-/g, ' ')
+        .replace(/\b\w/g, char => char.toUpperCase())
+}
+
+//Error message 
 const getData = async (url, errorMessage = 'Something went wrong') => {
     const response = await fetch(url);
     if (!response.ok) {
@@ -10,6 +18,7 @@ const getData = async (url, errorMessage = 'Something went wrong') => {
     return response.json();
 };
 
+//Function to show the latest project
 const fetchLatestProject = async () => {
     loadingState.classList.remove('hidden')
 
@@ -17,11 +26,11 @@ const fetchLatestProject = async () => {
         const repos = await getData(gitHubApiUrl)
 
         const latestRepo = repos[0]
-
+        //Name, description, demo, links
         latestProjectContainer.innerHTML = `
              <h2>Latest Project</h2>
 
-    <h3>${latestRepo.name}</h3>
+    <h3>${formatRepoName(latestRepo.name)}</h3>
     <p>${latestRepo.description || 'No description provided.'}</p>
 
     ${latestRepo.homepage ? `
@@ -33,17 +42,18 @@ const fetchLatestProject = async () => {
     <a href="${latestRepo.html_url}" target="_blank">GitHub</a>
     ${latestRepo.homepage ? ` | <a href="${latestRepo.homepage}" target="_blank">Live Demo</a>` : ''}
         `
-
+        //Replace them with some comment if not available
     } catch (error) {
         latestProjectContainer.innerHTML = `
             <h2>Latest Project</h2>
             <p>${error.message}</p>
         `
+        //Loading state 
     } finally {
         loadingState.classList.add('hidden')
     }
 }
-
+//Call and show the function (latest project)
 window.addEventListener('DOMContentLoaded', () => {
     fetchLatestProject();
 })
